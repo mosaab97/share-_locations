e0397868dfb9cec1c995dbf74c2c8f129f7c1213
MosaabAL_Dawood <maldawod@aspire-services.net>
2025-09-06 18:06:24 -0500

    connect to mongo db

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
 const placesRoutes = require('./routes/places');
 const usersRoutes = require('./routes/users');
 const HttpError = require('./models/httpError');
@@ -28,6 +29,14 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500).json({ message: error.message || 'An unknown error occurred!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const dbConnectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_APP_NAME}.xejq0zk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`;
+mongoose.connect(dbConnectionString)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log('Failed to connect to MongoDB', err);
+  });
